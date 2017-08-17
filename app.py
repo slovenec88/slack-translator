99858314f1a45f9c2d065b1feea39e2c52911d52
slovenec88 <slovenec88@gmail.com>
2017-08-17 16:04:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,8 @@ def google_translate1(text, from_, to):
         headers = {'User-Agent': 'Mozilla/5.0 (compatible; Google-Apps-Script)', 'Accept-Encoding': 'gzip,deflate,br'}
         r = requests.get(
             'https://eafc9936.ngrok.io?client=gtx&sl={}&tl={}&dt=t&q={}'.format('auto', to, text), headers=headers).json()
-        return r
+        post_to_slack(r[0][0][0])
+        return r[0][0][0]
     except Exception as e:
         post_to_slack(e)
 
@@ -97,7 +98,6 @@ def get_user(user_id):
 def translate_and_send(user_id, user_name, channel_id, text, from_, to):
     post_to_slack(text)
     translated = google_translate1(text, from_, to)
-    post_to_slack(translated)
     user = get_user(user_id)
     try:
         for txt in (text, translated):
